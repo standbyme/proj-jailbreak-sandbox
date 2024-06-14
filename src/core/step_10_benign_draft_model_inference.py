99390ab7796b9600476bc449c3cf7d54b6379bfe
Hongyu Cai <h.tsai@hotmail.com>
2024-06-14 11:50:16 -0400

    step 10

--- src/core/step_10_benign_draft_model_inference.py
+++ src/core/step_10_benign_draft_model_inference.py
@@ -3,6 +3,7 @@ import pickle
 import sys
 from pathlib import Path
 import time
+import unittest
 from tqdm import tqdm
 
 
@@ -15,62 +16,91 @@ def add_proj_to_PYTHONPATH():
 add_proj_to_PYTHONPATH()
 from src.core.language_model import HuggingFaceLanguageModel
 from src.core.utils import get_model_id
-from src.core.guard import (
-    BatchEvaluation,
-    LlamaGuardBatchEvaluation,
-)
 
 
-def handle_intent(
-    intent: str, draft_model: HuggingFaceLanguageModel, guard: BatchEvaluation
+def handle_dataset(
+    step_9_pickle_data,
+    result_path,
+    target_model: HuggingFaceLanguageModel,
+    dataset_name: str,
 ):
-    prompt = intent
-    draft_number = 25
-    # use draft_model get 35 responses for intent
-    responses = draft_model.inference(
-        prompt, do_sample=True, max_new_tokens=128, num_return_sequences=draft_number
-    )
-    assert len(responses) == draft_number
+    intents = step_9_pickle_data
+    checkpoint = []
 
-    # use guard to evaluate the responses
-    evaluation_results = guard.evaluate(prompt, responses)
-    unsafe_flag = any(evaluation_results)
+    for intent in tqdm(intents):
+        start_time = time.perf_counter()
+        responses = target_model.inference(
+            intent,
+            do_sample=True,
+            max_new_tokens=128,
+            num_return_sequences=draft_number,
+        )
+        end_time = time.perf_counter()
+        inference_time = end_time - start_time
 
-    if unsafe_flag:
-        print(f"Intent: {intent} is unsafe", flush=True)
+        assert len(responses) == draft_number
+
+        checkpoint.append(
+            {"intent": intent, "responses": responses, "time": inference_time}
+        )
+
+    with open(result_path / f"{dataset_name}.pkl", "wb") as f:
+        pickle.dump(checkpoint, f)
 
 
 def main():
-    draft_model_name = "opt-125m-AWQ"
+    step_9_benign_dataset_clean_result_path = Path().cwd() / "step_9_result"
+
+    cleaned_dataset_file_path_list = list(
+        step_9_benign_dataset_clean_result_path.iterdir()
+    )
+    cleaned_dataset_file_path_list.sort()
+
+    file_path = cleaned_dataset_file_path_list[slurm_unit_index]
+
+    dataset_name = file_path.name.split(".")[0]
+
+    result_path = Path().cwd() / "step_10_result" / draft_model_name / f"{draft_number}"
+    result_path.mkdir(parents=True, exist_ok=True)
+
+    with open(
+        file_path,
+        "rb",
+    ) as f:
+        step_9_pickle_data = pickle.load(f)
+
     draft_model_id = get_model_id(draft_model_name)
     draft_model = HuggingFaceLanguageModel(draft_model_id)
     draft_model.warm_up()
 
-    guard = LlamaGuardBatchEvaluation()
-    guard.set_is_consider_response(True)
-    guard.warmup()
-
-    # get the list of files in /depot/antoniob/data/hongyu/benchmark_dataset/MMLU_train/ with the extension .pkl
-    benchmark_dataset_path = Path(
-        "/depot/antoniob/data/hongyu/benchmark_dataset/MMLU_train"
-    )
-
-    for file_name in [
-        # "arc_easy.csv_train.pkl",
-        # "arc_hard.csv_train.pkl",
-        # "aux_law_90s.csv_train.pkl",
-        # "mc_test.csv_train.pkl",
-        # "race.csv_train.pkl",
-        "science_elementary.csv_train.pkl",
-        "science_middle.csv_train.pkl",
-    ]:
-        print(file_name, flush=True)
-        file_path = benchmark_dataset_path / file_name
-        with open(file_path, "rb") as f:
-            intents = pickle.load(f)
-            for intent in tqdm(intents):
-                handle_intent(intent, draft_model, guard)
+    handle_dataset(step_9_pickle_data, result_path, draft_model, dataset_name)
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--slurm_unit_index",
+        type=int,
+        required=True,
+    )
+    parser.add_argument(
+        "--draft_model_name",
+        type=str,
+        required=True,
+        choices=["opt-125m-AWQ"],
+    )
+    parser.add_argument(
+        "--draft_number",
+        type=int,
+        required=True,
+    )
+
+    args = parser.parse_args()
+    slurm_unit_index = args.slurm_unit_index
+    draft_model_name = args.draft_model_name
+    draft_number = args.draft_number
+
+    tc = unittest.TestCase()
+
     main()
