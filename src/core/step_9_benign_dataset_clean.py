084b1574cd2ca34973a5f9ce50bf1461647a5715
Hongyu Cai <h.tsai@hotmail.com>
2024-10-12 15:59:51 -0400

    step 9 checkpoint

--- src/core/step_9_benign_dataset_clean.py
+++ src/core/step_9_benign_dataset_clean.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 import pickle
 import sys
 import unittest
@@ -19,54 +20,79 @@ from src.core.language_model import HuggingFaceLanguageModel
 from src.core.utils import get_model_id
 
 
+def save_checkpoint(checkpoint_file_path, checkpoint):
+    with open(checkpoint_file_path, "wb") as f:
+        pickle.dump(checkpoint, f)
+
+
 def handle_dataset(
     dataset_file_path: Path,
-    result_path: Path,
+    result_dir_path: Path,
     target_model: HuggingFaceLanguageModel,
     guard: BatchEvaluation,
 ):
     tc.assertEqual(dataset_file_path.suffix, ".pkl")
 
-    result = []
+    checkpoint_file_path = (
+        result_dir_path / f"tmp.{slurm_unit_index}_{dataset_file_path.name}"
+    )
+    result_file_path = result_dir_path / f"{slurm_unit_index}_{dataset_file_path.name}"
+
+    if result_file_path.exists():
+        return
+
+    if checkpoint_file_path.exists():
+        with open(checkpoint_file_path, "rb") as f:
+            checkpoint = pickle.load(f)
+    else:
+        checkpoint = {
+            "state": 0,
+            "data": [],
+        }
 
     with open(dataset_file_path, "rb") as f:
         intents = pickle.load(f)
-        for intent in tqdm(intents):
-            guard.set_is_consider_response(False)
-            evaluation_results = guard.evaluate(intent, [])
-            tc.assertEqual(len(evaluation_results), 1)
 
-            evaluation_result = evaluation_results[0]
+    for intent in tqdm(intents[checkpoint["state"] :]):
+        checkpoint["state"] += 1
 
-            if evaluation_result:
-                continue
+        guard.set_is_consider_response(False)
+        evaluation_results = guard.evaluate(intent, [])
+        tc.assertEqual(len(evaluation_results), 1)
 
-            responses = target_model.inference(
-                intent, do_sample=True, max_new_tokens=128, num_return_sequences=1
-            )
-            assert len(responses) == 1
+        evaluation_result = evaluation_results[0]
 
-            guard.set_is_consider_response(True)
-            evaluation_results = guard.evaluate(intent, responses)
-            tc.assertEqual(len(evaluation_results), 1)
+        if evaluation_result:
+            save_checkpoint(checkpoint_file_path, checkpoint)
+            continue
 
-            evaluation_result = evaluation_results[0]
+        responses = target_model.inference(
+            intent, do_sample=True, max_new_tokens=128, num_return_sequences=1
+        )
+        assert len(responses) == 1
 
-            if evaluation_result:
-                continue
+        guard.set_is_consider_response(True)
+        evaluation_results = guard.evaluate(intent, responses)
+        tc.assertEqual(len(evaluation_results), 1)
 
-            result.append(intent)
+        evaluation_result = evaluation_results[0]
 
-    result_file_path = result_path / f"{slurm_unit_index}_{dataset_file_path.name}"
-    with open(result_file_path, "wb") as f:
-        pickle.dump(result, f)
+        if evaluation_result:
+            save_checkpoint(checkpoint_file_path, checkpoint)
+            continue
+
+        checkpoint["data"].append(intent)
+        save_checkpoint(checkpoint_file_path, checkpoint)
+
+    tc.assertEqual(len(intents), checkpoint["state"])
+    os.rename(checkpoint_file_path, result_file_path)
 
 
 def main():
     dataset_dir_path = Path("/depot/zcelik/data/hongyu/sandbox/benign/prompts")
 
-    result_path = Path().cwd() / "step_9_result" / target_model_name
-    result_path.mkdir(parents=True, exist_ok=True)
+    result_dir_path = Path().cwd() / "step_9_result" / target_model_name
+    result_dir_path.mkdir(parents=True, exist_ok=True)
 
     dataset_file_path_list = list(dataset_dir_path.iterdir())
     dataset_file_path_list.sort()
@@ -79,7 +105,7 @@ def main():
     target_model_id = get_model_id(target_model_name)
     target_model = HuggingFaceLanguageModel(target_model_id)
 
-    handle_dataset(file_path, result_path, target_model, guard)
+    handle_dataset(file_path, result_dir_path, target_model, guard)
 
 
 if __name__ == "__main__":
