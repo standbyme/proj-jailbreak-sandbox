aa86f076d69eec8335e9ae4765f58910e3fd0913
Hongyu Cai <h.tsai@hotmail.com>
2024-11-01 12:47:22 -0400

    collect_leaf_dirs

--- workdir/temp_copy.py
+++ workdir/temp_copy.py
@@ -1,6 +1,22 @@
-# pip install 'transformers>=4.39.1'
-from transformers import AutoModelForCausalLM, AutoTokenizer
+import os
 
-model_id = "CohereForAI/c4ai-command-r-v01-4bit"
-tokenizer = AutoTokenizer.from_pretrained(model_id)
-model = AutoModelForCausalLM.from_pretrained(model_id)
+
+def collect_leaf_dirs(path):
+    leaf_dirs = []
+
+    for root, dirs, files in os.walk(path):
+        # If there are no subdirectories, it's a leaf directory
+        if not dirs:
+            leaf_dirs.append(root)
+
+    return leaf_dirs
+
+
+# Example usage
+path = "/scratch/gilbreth/hongyu/project/sandbox/proj-jailbreak-sandbox/workdir/step_6_result"
+leaf_directories = collect_leaf_dirs(path)
+for leaf_dir in leaf_directories:
+    # get the file count in the leaf directory
+    file_count = len(os.listdir(leaf_dir))
+    if file_count < 50:
+        print(leaf_dir, file_count)
