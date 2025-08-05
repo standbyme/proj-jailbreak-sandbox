b456443153b1378586ff414401622856d763550c
Hongyu Cai <h.tsai@hotmail.com>
2025-08-05 19:25:26 -0400

    Anonymous

--- src/core/step_9_benign_target_model_inference.py
+++ src/core/step_9_benign_target_model_inference.py
@@ -52,7 +52,7 @@ def handle_dataset(
 
 
 def main():
-    dataset_dir_path = Path("/depot/zcelik/data/hongyu/sandbox/benign/just-eval")
+    dataset_dir_path = Path("/depot/anonymous/data/anonymoush/sandbox/benign/just-eval")
 
     dataset_file_path_list = list(dataset_dir_path.iterdir())
     dataset_file_path_list.sort()
--- src/slurm/10bdmi.tpl
+++ src/slurm/10bdmi.tpl
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-#SBATCH --mail-user=hongyu@purdue.edu
+#SBATCH --mail-user=anonymoush@purdue.edu
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-type=REQUEUE
